0704c28c0b1b20f19a1960c1efd141d8146128c9
RyanBarbalho <rgtbc@ic.ufal.br>
2023-11-14 10:17:01 -0300

    criado serializer de escola e de diretor da escola

--- api/serializers.py
+++ api/serializers.py
@@ -2,6 +2,14 @@ from rest_framework import serializers
 
 from api.models import *
 
+# responsible for creating school and principal
+
+
+class SchoolSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = School
+        fields = "__all__"
+
 
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
@@ -12,6 +20,9 @@ class StudentSerializer(serializers.ModelSerializer):
             "email",
             "semester",
         )
+        extra_kwargs = {
+            "password": {"write_only": True},
+        }
 
 
 class TeacherSerializer(serializers.ModelSerializer):
@@ -23,6 +34,34 @@ class TeacherSerializer(serializers.ModelSerializer):
             "email",
             "semester",
         )
+
+
+""" responsible for creating school and principal """
+
+
+class PrincipalSerializer(serializers.Serializer):
+    school = SchoolSerializer()
+    Principal = TeacherSerializer()
+
+    def create(self, validated_data):
+        school_data = validated_data["school"]
+        principal_data = validated_data["principal"]
+
+        school, principal = School.objects.create_school(
+            school_name=school_data.get("name"),
+            school_address=school_data.get("address"),
+            school_phone=school_data.get("phone"),
+            principal_name=principal_data.get("name"),
+            principal_email=principal_data.get("email"),
+            principal_password=principal_data.get("password"),
+            principal_phone=principal_data.get("phone"),
+            principal_address=principal_data.get("address"),
+        )
+        return {"school": school, "principal": principal}
+
+    # por enquanto, não é necessário atualizar o principal
+    def update(self, instance, validated_data):
+        raise NotImplementedError("Cannot call update() on an account")
 
 
 class CourseSerializer(serializers.ModelSerializer):
