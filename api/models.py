338bfcb82ca15db3a027586c7fa99c65ce12c17b
RyanBarbalho <rgtbc@ic.ufal.br>
2023-11-14 09:48:37 -0300

    models alterado, school criada

--- api/models.py
+++ api/models.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.models import AbstractUser, Group, Permission
-from django.db import models
+from django.db import models, transaction
 
 
-class CustomUser(AbstractUser, models.Model):
+class CustomUser(AbstractUser):
     name = models.CharField(max_length=50)
     email = models.EmailField(max_length=254)
     password = models.CharField(max_length=50)
@@ -15,16 +15,76 @@ class CustomUser(AbstractUser, models.Model):
     )
 
 
-class Student(CustomUser):
-    semester = models.IntegerField()
+class Principal(models.Manager):
+    @transaction.atomic
+    def create_school(
+        self,
+        school_name,
+        school_address,
+        school_phone,
+        principal_name,
+        principal_email,
+        principal_password,
+        principal_phone,
+        principal_address,
+    ):
+        school = School(
+            name=school_name,
+            address=school_address,
+            phone=school_phone,
+        )
+        school.save()
+
+        principal = Principal.objects.create_user(
+            name=principal_name,
+            email=principal_email,
+            password=principal_password,
+            phone=principal_phone,
+            address=principal_address,
+            school=school,
+        )
+        return school, principal
+
+
+class School(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    phone = models.IntegerField()
+
+    objects = Principal()
+
+    class Meta:
+        db_table = "schools"
 
     def __str__(self):
         return self.name
 
 
 class Teacher(CustomUser):
+    school = models.ForeignKey(
+        School, on_delete=models.CASCADE, default=None
+    )  # many to one relation between school and teacher
+    # decidi fazer many to one pq um professor pode dar aula em mais de uma escola mas
+    # n seria com a mesma conta no sistema
+
     def __str__(self):
         return self.name
+
+    class Meta:
+        db_table = "teachers"
+
+
+class Student(CustomUser):
+    semester = models.IntegerField()
+    school = models.ForeignKey(
+        School, on_delete=models.CASCADE, default=None
+    )  # many to one relation between school and student
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = "students"
 
 
 class Course(models.Model):
@@ -54,7 +114,7 @@ class AttendanceReport(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     total_lecture = models.IntegerField()
-    attendance = models.IntegerField()
+    attendance = models.IntegerField(default=0)
 
     def __str__(self):
         return self.student.name
